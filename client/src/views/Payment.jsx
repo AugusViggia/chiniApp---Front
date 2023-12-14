338aad6414f01cab0ea0ec3bf191909f04f6d057
Augusto <augusto.viggiano@gmail.com>
2023-12-14 16:26:33 -0300

    deploy update, Modal added

--- client/src/views/Payment.jsx
+++ client/src/views/Payment.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import { useNavigate } from 'react-router';
+import axios from "axios";
+
+const apiURL = "https://chiniapp-api-production.up.railway.app";
 
 const Payment = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // Simulación de lógica para verificar si el pago fue aprobado
+            const isPaymentApproved = true; // Puedes ajustar esto según tu lógica real
+
+            if (isPaymentApproved) {
+                try {
+                    // Hacer una solicitud al servidor para ejecutar la lógica de successEvent
+                    const response = await axios.post(
+                        `${apiURL}/success`,
+                        data
+                    );
+
+                    // Manejar la respuesta del servidor si es necesario
+                    console.log("Respuesta del servidor:", response.data);
+
+                    // Después, redirigir a /home
+                    navigate("/home");
+                } catch (error) {
+                    // Manejar errores si es necesario
+                    console.error("Error en la solicitud al servidor:", error);
+                }
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
-            <h2>Processing Payment</h2>
+            <h1>Procesando el pago...</h1>
+            {/* Puedes agregar más contenido según sea necesario */}
         </div>
-    )
+    );
 };
 
 export default Payment;
