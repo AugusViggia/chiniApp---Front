bd3e57233f6592cd1ec9604c2c8f3733b75bb8cf
JuanCruz <juancruzgenovese22@gmail.com>
2024-02-20 19:20:51 -0300

    Estilos en NavBar y Home

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,38 +1,43 @@
 import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, /*useLocation, useNavigate*/ } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBackwardStep,
   faShoppingCart,
   faHome,
-  faBars,
 } from "@fortawesome/free-solid-svg-icons";
 import style from "./NavBar.module.css";
 
 const NavBar = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-
-  const showBackButton = location.pathname !== "/";
+    //const location = useLocation();
+    //const navigate = useNavigate();
 
     return (
       <nav className={style.navbar}>
-        {showBackButton && (
-          <Link className={style.navLink} onClick={() => navigate(-1)}>
-            <FontAwesomeIcon icon={faBackwardStep} />
+        <div className={style.buttons}>
+          <Link to="/" className={style.navLink}>
+            <div className={style.btn}>
+              <span> Nosotros </span>
+            </div>
           </Link>
-        )}
-
-        <Link to="/products" className={style.navLink}>
-          <FontAwesomeIcon icon={faBars} />
-        </Link>
-
+          
+          <Link to="/products" className={style.navLink}>
+            <div className={style.btn}>
+              <span> Cocina </span>
+            </div>
+          </Link>
+        </div>
+        
         <Link to="/" className={style.navLink}>
-          <FontAwesomeIcon icon={faHome} />
+          <div className={style.home}>
+            <span className={style.logo}> ChiniBakery.</span>
+          </div>
         </Link>
 
         <Link to="/cart" className={style.navLink}>
-          <FontAwesomeIcon icon={faShoppingCart} />
+          <div className={style.cart}>
+            <FontAwesomeIcon icon={faShoppingCart}/>
+          </div>
+          
         </Link>
       </nav>
     );
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -4,14 +4,7 @@ import style from "./Home.module.css";
 function Home() {
   return (
     <div className={style.mainContainer}>
-      <h1>Bienvenido a chiniBakery</h1>
-
-      <p>
-        ABOUT US: Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-        veritatis eveniet sapiente, illum nihil praesentium vitae perferendis,
-        asperiores laudantium nobis velit quaerat tempora explicabo quas
-        blanditiis. Totam dolorem cupiditate praesentium.
-      </p>
+      <h1>Bienvenido</h1>
     </div>
   );
 }
