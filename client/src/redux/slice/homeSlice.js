c56557875a7a156a442351e3d6354262612b163a
Augusto <augusto.viggiano@gmail.com>
2024-02-23 09:51:11 -0300

    localstorage done

--- client/src/redux/slice/homeSlice.js
+++ client/src/redux/slice/homeSlice.js
@@ -1,43 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const homeSlice = createSlice({
-    name: "home",
-    initialState: {
-        allProducts: [],
-        cartList: [],
-    },
-    reducers: {
-        setSetectedProduct: (state, action) => {
-            state.selectedProduct = action.payload;
-        },
-        getProductById: (state, action) => {
-            const product = state.allProducts.find(
-                (product) => product.id === action.payload
-            );
-            return product;
-        },
-        addToCart: (state, action) => {
-            state.cartList.push(action.payload);
-        },
-        updateQuantity: (state, action) => {
-            const { productId, quantity } = action.payload;
-            const productIndex = state.cartList.findIndex(
-                (product) => product.id === productId
-            );
+const savedCart = JSON.parse(localStorage.getItem("cart"));
 
-            if (productIndex !== -1) {
-                state.cartList[productIndex].quantity = quantity;
-            }
-        },
-        removeFromCart: (state, action) => {
-            state.cartList = state.cartList.filter(
-                (item) => item.id !== action.payload.id
-            );
-        },
-        emptyCart: (state, action) => {
-            state.cartList = [];
-        },
+const homeSlice = createSlice({
+  name: "home",
+  initialState: {
+    allProducts: [],
+    cartList: savedCart || [],
+  },
+  reducers: {
+    setSetectedProduct: (state, action) => {
+      state.selectedProduct = action.payload;
     },
+    getProductById: (state, action) => {
+      const product = state.allProducts.find(
+        (product) => product.id === action.payload
+      );
+      return product;
+    },
+    addToCart: (state, action) => {
+      state.cartList = [...state.cartList, action.payload];
+    },
+    updateQuantity: (state, action) => {
+      const { productId, quantity } = action.payload;
+      const productIndex = state.cartList.findIndex(
+        (product) => product.id === productId
+      );
+
+      if (productIndex !== -1) {
+        state.cartList[productIndex].quantity = quantity;
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.cartList = state.cartList.filter(
+        (item) => item.id !== action.payload.id
+      );
+    },
+    emptyCart: (state, action) => {
+      state.cartList = [];
+    },
+  },
 });
 
 export const {
