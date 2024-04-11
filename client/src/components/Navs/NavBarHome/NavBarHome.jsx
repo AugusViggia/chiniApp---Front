1c28cf08462f8a3856128c9d321a2290daa50023
JuanCruz <juancruzgenovese22@gmail.com>
2024-04-10 21:43:06 -0300

    .

--- client/src/components/Navs/NavBarHome/NavBarHome.jsx
+++ client/src/components/Navs/NavBarHome/NavBarHome.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 //Importacion de componente Cart
-import Cart from "../../views/Cart/Cart";
+import Cart from "../../../views/Cart/Cart";
 //Importacion de estilos
 import style from "./NavBarHome.module.css";
 //Dependencias de GSAP
